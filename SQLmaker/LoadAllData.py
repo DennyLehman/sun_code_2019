029c501a60e5bf8423809867aaa09fef344c8dd9
Denny <dendenlehman@gmail.com>
2019-05-18 15:07:36 -0500

    updates

---
+++ SQLmaker/LoadAllData.py
@@ -0,0 +1,30 @@
+from DataLoader import DataLoader
+
+class AllDataLoader(DataLoader):
+	"""docstring for AllDataLoader"""
+	def __init__(self):
+		super(AllDataLoader, self).__init__()
+		self.file_name = 'Powerhouse_SunCode_SVCE_Data.csv'
+
+	def main(self):
+		file_path = self.get_file_path(file_name='Powerhouse_SunCode_SVCE_Data.csv',sub_directory='raw_data')
+
+		d = self.read_csv_data_from_file(file_name=file_path)
+		return d
+		
+
+
+
+if __name__ == '__main__':
+	adl = AllDataLoader()
+	adl.main()
+
+	# check with marta on map overlay
+	# who - introduce population, show dash
+		# low income\
+		# high gas usage
+	# where are they - show map
+	# what do they need - show need
+	# whats the solution - policy, money, whatever
+
+
--- SQLmaker/dbmaker.py
+++ SQLmaker/dbmaker.py
@@ -1,15 +1,34 @@
+import psycopg2
+#from config import config
+from DataLoader import DataLoader
 
-commands = (
-	"""
-	""",
-	"""
-	CREATE TABLE retail_energy_usage
-	(
-		row_id serial,
-		house_id varchar,
-		month int,
-		kwh int
-		
-	)
-	"""
-	)
+class MakeDB(DataLoader):
+	"""docstring for MakeDB"""
+	def __init__(self):
+		super(MakeDB, self).__init__()
+
+	def print_hi(self):
+		print('hi')
+
+	def make_table(self):
+		commands = (
+		""" 
+		DROP TABLE retail_energy_usage;
+		""",
+		"""
+		CREATE TABLE retail_energy_usage
+		(
+			row_id serial,
+			house_id varchar,
+			month int,
+			kwh int
+			
+		)
+		"""
+		)
+		pass
+
+if __name__ == '__main__':
+	
+	db = MakeDB()
+	db.print_hi()
