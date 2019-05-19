e9cc8d589007a29df3cc900d0b8488e2f871a40c
DNA <dreaderr@gmail.com>
2019-05-18 19:50:17 -0500

    Comments and deletes

--- SQLmaker/LoadAllData.py
+++ SQLmaker/LoadAllData.py
@@ -4,6 +4,9 @@ import random
 
 class AllDataLoader(DataLoader):
 	"""docstring for AllDataLoader"""
+
+	#Import of base data for analysis. This table is extendable as our data sources grow.
+	
 	def __init__(self):
 		super(AllDataLoader, self).__init__()
 		self.file_name = 'Powerhouse_SunCode_SVCE_Data.csv'
@@ -18,6 +21,8 @@ class AllDataLoader(DataLoader):
 
 		return d[1:]
 
+	#Pivot the gas and CSC data for easier analysis, so we can check data by month per house.. 
+
 	def pivot_gas(self):
 		file_name = self.get_file_path(file_name=self.file_name,sub_directory='raw_data')
 		months = ['thrm_jan','thrm_feb','thrm_mar','thrm_apr','thrm_may','thrm_jun','thrm_jul','thrm_aug','thrm_sep','thrm_oct','thrm_nov','thrm_dec']
@@ -28,17 +33,16 @@ class AllDataLoader(DataLoader):
 	def pivot_electric(self):
 		file_name = self.get_file_path(file_name='Powerhouse_SunCode_SVCE_Data.csv',sub_directory='raw_data')
 		months = ['KWH_JAN_2018','KWH_FEB_2018','KWH_MAR_2018','KWH_APR_2018','KWH_MAY_2018','KWH_JUN_2018','KWH_JUL_2018','KWH_AUG_2018','KWH_SEP_2018','KWH_OCT_2018','KWH_NOV_2018','KWH_DEC_2018']
-		#l = self.pivot_month_data(file_name, months)
-		
+			
 		pass
 	
 
 	def pivot_month_data(self,file_name,month_columns):
-		#df = pd.read_csv(self.get_file_path(file_name='Powerhouse_SunCode_SVCE_Data.csv',sub_directory='raw_data'))
+		)
 		df = pd.read_csv(file_name)
-		#months = ['thrm_jan','thrm_feb','thrm_mar','thrm_apr','thrm_may','thrm_jun','thrm_jul','thrm_aug','thrm_sep','thrm_oct','thrm_nov','thrm_dec']
+		
 		i = 1
-		#new_df = pd.DataFrame(columns = ['1','2','3','4','5','6','7','8','9','10','11','12'])
+		
 		new_df = pd.DataFrame(columns=['A'])
 		for m in month_columns:
 			print(i,m)
@@ -63,24 +67,37 @@ class AllDataLoader(DataLoader):
 		INSERT INTO thermal_data (house_id, month, thermal) VALUES 
 		"""
 		for d in data:
-			#print(d)
+			
 			sql += '({},{},{}), '.format(d[0],d[1],d[2])
 
 		return sql[:-2]
 
+	#Creating Zip codes for o ur data. This data would be provided by survey information and community outreach.
+
 	def get_zip_code(self,city):
 		
 		sunnyvale = ['94085','94086','94088','94089']
+		
 		saratoga = ['95070','95071']
+		
 		los_altos = ['94022','94023','94024']
+		
 		campbell = ['95008','95009','95011']
+		
 		gilroy = ['95020', '95021']
+		
 		santa_clara_unincorp = ['95050','95051','95052','95053','95054','95055','95056']
+		
 		cupertino = ['94024','94087','95014','95015']
+		
 		mountain_view = ['94035','94036','94037','94038','94039','94040','94041','94042','94043']
+		
 		milpitas = ['95035','95036']
+		
 		morgan_hill = ['95037','95038']
+		
 		monte_sereno = ['95030']
+		
 		los_gatos = ['95030','95031','95032','95033']
 
 		d ={
@@ -89,7 +106,7 @@ class AllDataLoader(DataLoader):
 			}
 
 		ran = random.randint(0,len(d[city])-1)
-		#print(city, ran)
+		
 		_zip = d[city][ran]
 
 
@@ -97,6 +114,8 @@ class AllDataLoader(DataLoader):
 
 
 	
+	#This automatically imports the survey data from community outreach progams including education outreach and teacher outreach.
+	#This data would then automatically update on the map for review and targeted efficiency.
 
 	def load_survey_results(self):
 		pass
@@ -120,7 +139,7 @@ class AllDataLoader(DataLoader):
 		INSERT INTO thermal_data (house_id, month, thermal) VALUES 
 		"""
 		for d in data:
-			#print(d)
+			
 			sql += '({},{},{}), '.format(d[0],d[1],d[2])
 
 		return sql[:-2]
@@ -161,17 +180,7 @@ class AllDataLoader(DataLoader):
 if __name__ == '__main__':
 	adl = AllDataLoader()
 	adl.load_table_house_data()
-	#print(adl.pivot_month_data()[0:5])
+	
 	adl.run_gas_data()
-	#adl.pivot_gas()
-	#print(adl.get_zip_code('CAMPBELL'))
-
-	# check with marta on map overlay
-	# who - introduce population, show dash
-		# low income\
-		# high gas usage
-	# where are they - show map
-	# what do they need - show need
-	# whats the solution - policy, money, whatever
-
+	
 
